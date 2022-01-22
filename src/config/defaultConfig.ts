b7dcb21778a624734abb62b830541521d6064845
clanguo <1642805102@qq.com>
2022-01-22 21:55:46 +0800

    修改网站默认允许跨域的范围

---
+++ src/config/defaultConfig.ts
@@ -0,0 +1,15 @@
+export default {
+  "allowOrigin": [
+    "http://localhost:3000/",
+    "http://localhost:3001/",
+    "http://47.97.211.129/",
+    "http://47.97.211.129:80/",
+    "http://47.97.211.129:82/",
+    "http://clangup.top/",
+    "http://clangup.top:80/",
+    "http://blog.clangup.top/",
+    "http://blog.clangup.top:80/",
+    "http://clangup.top:82/",
+    "http://admin.clangup.top:82/"
+  ]
+}
--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,18 +1,22 @@
 import * as cors from "cors";
 import ConfigManager from "../config/configManager";
 import { CORSError } from "./errorMiddleware";
+import defaultConfig from "../config/defaultConfig";
 
 const configManager = ConfigManager.getConfigManager();
 
 export default cors({
   origin: (origin, callback) => {
     const allowOrigin = configManager.getConfig("allowOrigin");
-    if (typeof allowOrigin === "boolean") {
-      if (allowOrigin) {
-        callback(null, true);
-      }
+    if (typeof allowOrigin === "boolean" && allowOrigin === true) {
+      callback(null, true);
     } else {
+      /**
+       * 默认配置包含了本地服务器和部署服务器
+       */
       if (
+        defaultConfig.allowOrigin.includes(origin) ||
+        allowOrigin !== false ||
         allowOrigin && (allowOrigin as string[]).includes(origin)
         || origin === undefined
       ) {
